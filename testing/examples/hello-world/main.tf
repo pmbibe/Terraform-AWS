3590980439a6d2db0414f8bfada3dcebe65c4aed
pmbibe <ptminhduc18101997@gmail.com>
2023-06-29 05:41:41 +0000

    CD

--- testing/examples/hello-world/main.tf
+++ testing/examples/hello-world/main.tf
@@ -1,4 +1,13 @@
 terraform {
+
+  # backend "s3" {
+  #   bucket         = "devops-directive-tf-state"
+  #   key            = "examples/hello-world/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "terraform-state-locking"
+  #   encrypt        = true
+  # }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
